c21f01d5c5712ab2eab10a5715abe8dfe2420f0c
Mark Morrison <markmorrison058@gmail.com>
2020-03-06 18:51:44 +0000

    login opening but looks like trash

--- macro_mate/views.py
+++ macro_mate/views.py
@@ -3,6 +3,8 @@ from django.contrib.auth import authenticate, login
 from django.http import HttpResponse
 from django.urls import reverse
 from django.shortcuts import redirect 
+from macro_mate.models import UserProfile
+
 
 # Create your views here.
 
@@ -17,7 +19,7 @@ def user_login(request):
     if request.method == 'POST':
         username = request.POST.get('username')
         password = request.POST.get('password')
-        user.authenticate(username=username, password = password)
+        user = authenticate(username=username, password = password)
 
         # if the login details are correct 
         if user:
@@ -30,5 +32,5 @@ def user_login(request):
             print("Incorrect login details: {username}, {password}")
             return HttpResponse("Incorrect login details")
     else:
-        return render(request, 'macromate/login.html')
+        return render(request, 'macro_mate/login.html')
 
