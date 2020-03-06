abab3f02bf04da459b06a7c1dfaea27ce1b26a77
hughwin <hugh.winchester@gmail.com>
2020-03-06 17:40:08 +0000

    Cleaned up base class, and added meals.html

--- macro_mate/views.py
+++ macro_mate/views.py
@@ -2,8 +2,12 @@ from django.shortcuts import render
 
 # Create your views here.
 
-
 def index(request):
     context_dict = {}
     response = render(request, 'macro_mate/index.html', context=context_dict)
+    return response
+
+def meals(request):
+    context_dict = {}
+    response = render(request,  'macro_mate/meals.html', context=context_dict)
     return response
