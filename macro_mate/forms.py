5b650ce39e04b33354a9b056071a0b05e22103d0
Gareth Sears <garethsears@gmail.com>
2020-03-07 19:39:00 +0000

    fix codacy errors

--- macro_mate/forms.py
+++ macro_mate/forms.py
@@ -2,15 +2,16 @@ from django import forms
 from django.contrib.auth.models import User
 from macro_mate.models import User_Profile
 
-class UserForm(forms.ModelForm):
+
+class User_Form(forms.ModelForm):
     password = forms.CharField(widget=forms.PasswordInput())
 
     class Meta:
         model = User
         fields = ('username', 'email', 'password',)
 
-class User_ProfileForm(forms.ModelForm):
+
+class User_Profile_Form(forms.ModelForm):
     class Meta:
         model = User_Profile
         fields = ('profilePicture',)
-
