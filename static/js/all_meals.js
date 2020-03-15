51baf62ac91881cf21eac610c2e3e5b39eaaa5ef
Gareth Sears <garethsears@gmail.com>
2020-03-15 21:41:35 +0000

    add comments to all_meals javascript

--- static/js/all_meals.js
+++ static/js/all_meals.js
@@ -1,4 +1,13 @@
+/**
+ * A function which sets up the search text box using the MealAPI view in the
+ * API folder.
+ *
+ * @param {string} userID The user's ID so their meals can be fetched
+ * @param {string} mealURL The URL which links to the meal's page
+ * @param {string} apiURL The meal API URL which can fetch a user's / all meals
+ */
 function setupSearch(userID, mealURL, apiURL) {
+  // Initialise selectize plugin
   const $select = $("#search").selectize({
     options: [],
     maxItems: 1,
@@ -11,11 +20,13 @@ function setupSearch(userID, mealURL, apiURL) {
     createOnBlur: true,
     create: false,
     onChange: function(value) {
-      // Change window to the meal URL
+      // Change window to the meal URL on selection
       document.location.href = mealURL + value;
     },
+    // A custom render html for each item in the dropdown list.
     render: {
       option: function optionRenderer(item, escape) {
+        // escape is used for escaping dangerous html
         return (
           '<div class="list-group">' +
           '<a class="text-body list-group-item" href="' +
@@ -30,20 +41,15 @@ function setupSearch(userID, mealURL, apiURL) {
     },
   });
 
-  const $mealSearch = $select[0].selectize;
-  // The following code is needed to prevent event propogation
-  // so that the links will be registered on mouse up. Usually,
-  // selectize will long close the list before this.
-
-  // Ajax request to avoid getting tags from template
-  // (Probably unsafe to mix python and js... also good demo of ajax call)
-
+  // AJAX call to the meal api
   $.get({
     url: apiURL,
     data: {
       user: userID ? userID : null,
     },
   }).done(function(response) {
+    // Get the jquery plugin
+    const $mealSearch = $select[0].selectize;
     $mealSearch.clearOptions();
     $mealSearch.load(function(callback) {
       callback(response);
