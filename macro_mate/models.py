d129ba39b015401f744a1a1ad67d4e4c1c0b0078
Gareth Sears <garethsears@gmail.com>
2020-03-15 21:36:36 +0000

    codacy fixed

--- macro_mate/models.py
+++ macro_mate/models.py
@@ -10,31 +10,30 @@ from django.template.defaultfilters import slugify
 
 
 class UserProfile(models.Model):
-    """A user profile model linked to Django's base User class."""
+    """A user profile model linked to Django's base User. Application specific fields are added."""
 
-    # User
-    # ----
     # Link to the basic User account
     user = models.OneToOneField(User, on_delete=models.CASCADE)
 
     # Add user profile picture
-    picture = models.ImageField(
-        upload_to='profile_pictures', blank=True)
+    picture = models.ImageField(upload_to='profile_pictures',
+                                blank=True)
 
-    # Set profile name to the user name
+    # Set name to the user's name
     def __str__(self):
         return self.user.username
 
-# The following receivers synchronise User_Profile with User when a
-# new user is created or saved.
+
 @receiver(post_save, sender=User)
 def create_user_profile(sender, instance, created, **kwargs):
+    """Create a corresponding UserProfile object when a User is created"""
     if created:
         UserProfile.objects.create(user=instance)
 
 
 @receiver(post_save, sender=User)
 def save_user_profile(sender, instance, **kwargs):
+    """Save the userprofile accordingly when modifications to a user are made"""
     instance.userprofile.save()
 
 
@@ -43,10 +42,8 @@ def save_user_profile(sender, instance, **kwargs):
 
 # THIS MUST BE POPULATED WHEN DEPLOYING APP
 class MealCategory(models.Model):
-    """A model for holding a required category for a meal, e.g. breakfast"""
-    # Meal Category Choices
-    # ---------------------
-    # Uses MultiSelectField. Constants used to programatically create in populate script
+    """A model for holding a required category for a meal, e.g. breakfast.
+    This must be pre-populated prior to deployment."""
 
     CATEGORY_MAX_LENGTH = 12
 
@@ -62,6 +59,7 @@ class MealCategory(models.Model):
 
     slug = models.SlugField()
 
+    # Override save to create a slug automatically
     def save(self, *args, **kwargs):
         self.slug = slugify(self.category)
         super(MealCategory, self).save(*args, **kwargs)
@@ -72,9 +70,6 @@ class MealCategory(models.Model):
 
 class Meal(models.Model):
     """A model for a meal."""
-
-    # Constraints
-    # -----------
 
     NAME_MAX_LENGTH = 128
     URL_MAX_LENGTH = 255
@@ -88,39 +83,37 @@ class Meal(models.Model):
     # Reference Fields
     # ----------------
 
-    # Date and time created
+    # Date and time created / modified
     created_date = models.DateTimeField(auto_now_add=True)
     modified_date = models.DateTimeField(auto_now=True)
 
     # The owning user
-    # When the user is deleted, all their meals are deleted
-    owner = models.ForeignKey(
-        UserProfile, on_delete=models.CASCADE,  related_name='%(class)s_owner')
+    # on_delete: When the user is deleted, all their meals are deleted
+    owner = models.ForeignKey(UserProfile,
+                              on_delete=models.CASCADE,
+                              related_name='%(class)s_owner')
 
     # Users
     # Each user can have many meals in their DB, which aren't necessarily owned by them
-    users = models.ManyToManyField(UserProfile, related_name='%(class)s_users')
+    users = models.ManyToManyField(UserProfile,
+                                   related_name='%(class)s_users')
 
-    # Fields
-    # ------
     name = models.CharField(max_length=NAME_MAX_LENGTH)
 
-    # Categories
-    # ----------
     # This needs to be enforced to have MIN: 1 in the forms field
     categories = models.ManyToManyField(MealCategory)
 
-    # Tags
-    # ----
-    # Uses TaggableManager, which can add tags.
+    # Uses Django Taggit's TaggableManager, which can add tags.
+    # https://django-taggit.readthedocs.io/en/latest/
     tags = TaggableManager(blank=True)
 
     servings = models.IntegerField(default=1)
 
     # Ingredient field
     # ----------------
-    # Should be a comma / new line seperated list. Enforce at form level.
-    # May want to migrate this to another object, but currently not much need as API does heavy lifting.
+    # Should be a comma / new line seperated list.
+    # The API call will throw errors to the user and should disallow analysis if these
+    # cannot be parsed correctly.
     ingredients = models.TextField(blank=True)
 
     url = models.URLField(max_length=URL_MAX_LENGTH, blank=True)
@@ -129,20 +122,23 @@ class Meal(models.Model):
 
     # Nutrition Fields
     # ----------------
-    # These values are cached from the API, should NOT be visible to user
+    # These values are cached from the API, should NOT be visible to user in a form
 
-    calories_unit = models.CharField(
-        max_length=UNIT_MAX_LENGTH, default='kcal')
     calories_quantity = models.FloatField(default=0.0)
+    calories_unit = models.CharField(max_length=UNIT_MAX_LENGTH,
+                                     default='kcal')
 
-    fat_unit = models.CharField(max_length=UNIT_MAX_LENGTH, default='g')
     fat_quantity = models.FloatField(default=0.0)
+    fat_unit = models.CharField(max_length=UNIT_MAX_LENGTH,
+                                default='g')
 
-    protein_unit = models.CharField(max_length=UNIT_MAX_LENGTH, default='g')
     protein_quantity = models.FloatField(default=0.0)
+    protein_unit = models.CharField(max_length=UNIT_MAX_LENGTH,
+                                    default='g')
 
-    carbs_unit = models.CharField(max_length=UNIT_MAX_LENGTH, default='g')
     carbs_quantity = models.FloatField(default=0.0)
+    carbs_unit = models.CharField(max_length=UNIT_MAX_LENGTH,
+                                  default='g')
 
     image = models.ImageField(upload_to='meal_images', blank=True)
 
