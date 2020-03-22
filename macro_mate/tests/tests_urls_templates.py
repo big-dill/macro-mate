e6094bb69c35fce13dbda9a6e1e6d8dee70ccc0b
markmorrison95 <56168648+markmorrison95@users.noreply.github.com>
2020-03-22 16:53:37 +0000

    Added all the testing files in the folder macro_mate/tests (#23)
    * failing 2 test
    
    * all tests added
    
    * fixed issues
    
    * removing unused imports
    
    * removed import and trailing whitespace

---
+++ macro_mate/tests/tests_urls_templates.py
@@ -0,0 +1,35 @@
+from django.test import TestCase
+from django.contrib.auth.models import User
+from populate_macro_mate import populate
+
+
+def createUser():
+    user = User.objects.get_or_create(username='testuser',
+                                        email='test@test.com')[0]
+    user.set_password('testabc123')
+    user.save()
+    return user
+
+class tests_urls_templates(TestCase):
+
+    def test_url_usage(self):
+        """ A test for checking the correct page is loaded with each url"""
+        populate()
+        createUser()
+        self.client.login(username='testuser', password='testabc123')
+        # tests that all the urls used are associated with the right html
+        urls = ['/macro_mate/',
+            '/macro_mate/meal/12/',
+            '/macro_mate/meals/',
+            '/macro_mate/my_meals/',
+            '/macro_mate/add_meal/',]
+
+        templates = ['macro_mate/index.html',
+                'macro_mate/meal.html',
+                'macro_mate/all_meals.html',
+                'macro_mate/all_meals.html',
+                'macro_mate/add_meal.html',]
+
+        for url, template in zip(urls, templates):
+            response = self.client.get(url, follow=True)
+            self.assertTemplateUsed(response, template)
---
+++ macro_mate/tests/tests_user_registration.py
@@ -0,0 +1,41 @@
+from django.test import TestCase
+from django.contrib.auth.models import User
+from macro_mate.models import UserProfile
+
+
+class tests_user_registration(TestCase):
+
+
+    def test_bad_user_registration(self):
+        """ checks if when info is left black the post request doesn't go through """
+        response = self.client.post('/accounts/register/', {'username': '','email': '', 'password1': '', 'password2': ''})
+        self.assertTrue(response.status_code != 302)
+
+
+    def test_good_user_registration_view(self):
+        """ checks post request when info entered in registration view """
+        response = self.client.post('/accounts/register/', {'username': 'testuser', 'email': 'test@test.com','password1': 'testabc123','password2': 'testabc123'})
+        self.assertEqual(response.status_code, 302)
+
+    def tests_bad_user_login_view(self):
+        """ checks post request when info is left blank when attemting to login """
+        response = self.client.post('/accounts/login/', {'username': '', 'email': '','password': ''})
+        self.assertTrue(response.status_code != 302)
+
+
+    def test_good_form_creation(self):
+        """ Tests the functionality of the user registration forms.
+        Creates a UserProfileForm and UserForm, and attempts to save them.
+        tests login with the details if created properly also tests the login view """
+        user_data = {'username': 'testuser', 'email': 'test@test.com', 'password': 'testabc123'}
+
+        user = User.objects.get_or_create(username='testuser',
+                                            email='test@test.com')[0]
+        user.set_password('testabc123')
+        user.save()
+
+        response = self.client.post('/accounts/login/', user_data)
+        self.assertTrue(response.status_code == 302)
+        self.assertEqual(len(User.objects.all()), 1)
+        self.assertEqual(len(UserProfile.objects.all()), 1)
+        self.assertTrue(self.client.login(username='testuser', password='testabc123'))
